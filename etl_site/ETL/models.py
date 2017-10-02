89ef6f1fafd2d6980f7250c387082a46b8351da0
Matt <matt@matthewjorr.com>
2017-10-02 18:52:12 -0400

    switched to extend from abstractuser and added max lengths

--- etl_site/ETL/models.py
+++ etl_site/ETL/models.py
@@ -2,26 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(models.Model):
-	email = models.CharField()
-	ip = models.CharField()
-	signup_date = models.DateTimeField(auto_now_add=True)
-	status = models.CharField()
-	first_name = models.CharField()
-	last_name = models.CharField()
-	password = models.CharField()
-	occupation = models.CharField()
-	industry = models.CharField()
-	company = models.CharField()
-	registration_date = models.DateTimeField()
-	last_login = models.DateTimeField()
+class User(AbstractUser):
+    ip = models.CharField(max_length=39)
+    status = models.CharField(max_length=10)
+    occupation = models.CharField(max_length=50)
+    industry = models.CharField(max_length=50)
+    company = models.CharField(max_length=50)
+    date_registered = models.DateTimeField()
 
 class Table(models.Model):
-	id_user = models.IntegerField()
-	table_name = models.CharField()
-	date_created = models.DateTimeField(auto_now_add=True)
-	db_server = models.CharField()
-	reference_sheet = models.CharField()
-	last_modefied = models.DateTimeField(auto_now=True)
+    user_id = models.IntegerField()
+    table_name = models.CharField(max_length=144)
+    date_created = models.DateTimeField(auto_now_add=True)
+    db_server = models.CharField(max_length=144)
+    reference_sheet = models.CharField(max_length=144)
+    last_modefied = models.DateTimeField(auto_now=True)
--- etl_site/etl_site/settings.py
+++ etl_site/etl_site/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'elt_site.etl',
 ]
+
+AUTH_USER_MODEL = 'elt.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
